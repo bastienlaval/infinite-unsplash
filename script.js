9069fd49354786356f3e402a5f597af34437929e
Bastien Laval <bastienlaval@macbook-pro-de-bastien.home>
2020-07-31 22:29:42 +0200

    Ajout choix d'un thème

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 let photosArray = [];
+let themePhotos = "travel";
 
 // Unsplash API
-const count = 10;
+let count = 4;
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 // Normally, don't store API Keys like this, but an exception made here because it is free, and the data is publicly available!
 const apiKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=${themePhotos}`;
+
+// on vérifie si toutes les images sont bien chargées
+function imageLoaded() {
+  imagesLoaded++;
+  console.log("image loaded");
+  if (imagesLoaded === totalImages) {
+    console.log("ready = true");
+    ready = true;
+    loader.hidden = true;
+    count = 20;
+    apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=${themePhotos}`;
+  }
+}
+
 
 // Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
@@ -16,6 +34,8 @@ function setAttributes(element, attributes) {
   }
 
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach( (photo) => {
 
     // Création du lien vers le site unsplash
@@ -38,6 +58,8 @@ function displayPhotos() {
         title: photo.alt_description,
       });
 
+    // ajout de l'event listener pour s'assurer que l'image est chargée
+    img.addEventListener('load', imageLoaded);
     // on ajoute ces éléments au container
     item.appendChild(img);
     imageContainer.appendChild(item);
@@ -47,16 +69,44 @@ function displayPhotos() {
 
 // Get photos from Unsplash API
 async function getPhotos() {
+    // loader.hidden = false;
     try {
       const response = await fetch(apiUrl);
       photosArray = await response.json();
-      console.log(photosArray );
+      console.log(photosArray);
       displayPhotos();
     } catch (error) {
       // Catch Error Here
       console.log(error);
     }
   }
+
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// si sélection d'un thème
+
+function selectionTheme() {
+  //changedText.textContent = this.value;
+  console.log("Sélection du thème : " + this.value);
+  loader.hidden = false;
+  while (imageContainer.firstChild) {
+    imageContainer.removeChild(imageContainer.firstChild);
+  }
+  themePhotos = this.value;
+  count = 4;
+  apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}&query=${themePhotos}`;
+  getPhotos();
+
+}
+
+document.getElementById("slct").onchange = selectionTheme;
 
 
   // On Load
